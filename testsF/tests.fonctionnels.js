6e6943bc214b9a9b9f8481e6af54e89cfb4307bb
kyllianR <kyllian.rullier@epitech.eu>
2026-04-17 12:18:40 +0200

    ADD : test.fonctionnels, FIX : test

---
+++ testsF/tests.fonctionnels.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('Le joueur à 100 PV au début de la partie', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page.locator('.h')).toHaveText('100');
+});
+
+test('Game Over est caché au début de la partie', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page.locator('.e')).toHaveClass(/hidden/);
+});
+
+test('Le score est de 0 au début de la partie', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page.locator('.s')).toHaveText('0');
+});
